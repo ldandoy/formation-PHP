d37475d7b0848cf6b683ac250c60c08f8f8fa1f0
ldandoy <ldandoy@overconsulting.net>
2019-10-20 23:54:56 +0200

    Code de la partie hello world

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+<?php
+
+	// Ceci est mon premier fichier PHP
+	print("Test<br />");
+
+?>
+
+<?php
+	echo '<b>Ceci</b> est un "autre" test';
+?>
+	</body>
+</html>
