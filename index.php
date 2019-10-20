ea885d588b36ddf602d58f0aa5437d6c9303277b
ldandoy <ldandoy@overconsulting.net>
2019-10-21 00:16:12 +0200

    Code sur la partie variable

--- index.php
+++ index.php
@@ -5,14 +5,46 @@
 	</head>
 	<body>
 <?php
-
-	// Ceci est mon premier fichier PHP
-	print("Test<br />");
-
+/**
+ * Les différents types de variables en PHP
+ */
+	$mavariable = "toto"; # Chaine de caractère => String
+	$entier = 12; # Entier => Integer
+	$decimal = 34.25; # Décimal => Float
+	$boolean = true; # Boolean true|false ou 0|1
+	$tableau = array("Lion", "Banane", "Voiture") # Tableau => Array
 ?>
 
 <?php
-	echo '<b>Ceci</b> est un "autre" test';
+/**
+ * Utilisation des variables et des contantes
+ */
+$note = 10;
+$note = 15;
+
+echo $note.'<br />';
+
+define('CONTANTE', "Toto");
+echo CONTANTE;
+?>
+<hr />
+<?php
+/**
+ * Les opérateurs en PHP
+ */
+
+echo "toto " . $note . " titi<br />"; # Concaténation
+echo $note . " " . $entier . "<br />";
+echo $note + $entier . "<br />";
+echo $note - $entier . "<br />";
+echo $note * $entier . "<br />";
+echo $note / $entier . "<br />";
+echo 6 % 3 . "<br />";
+echo 7 % 3 . "<br />";
+
+echo 10 % 5 . "<br />";
+echo 30 % 5 . "<br />";
+
 ?>
 	</body>
 </html>
