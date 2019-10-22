d5ed6937297ab27c23c4ea4c330f28d3eb917b67
ldandoy <ldandoy@overconsulting.net>
2019-10-22 17:45:29 +0200

    Code sur la partie include

---
+++ fichier_inclus.php
@@ -0,0 +1,5 @@
+<?php
+
+function helloklm() {
+	echo "hello klm";
+}
---
+++ include.php
@@ -0,0 +1,11 @@
+<?php
+
+	echo "toto";
+	$charlotte = true;
+
+	require_once 'fichier_inclus.php';
+	helloklm();
+
+	require_once 'fichier_inclus.php';
+
+	echo "test";
